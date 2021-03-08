1c1d013a8387b6f7c886a702dbd5012fd50bf3f5
nazos <nicolas_nazos@hotmail.com>
2021-03-08 00:21:22 -0300

    changed template sunday

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
+from .models import Post
+
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(title__contains='no').order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def about(request):
